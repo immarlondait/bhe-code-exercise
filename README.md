bd3b4db649cd71105ba60a25a4e19707a057e878
Marlon Dait <immarlondait@gmail.com>
2026-05-04 10:19:53 -0500

    Cleaned up sieve code, added tests (fuzz, boundaries, file), added dockerfile, updated README.md

--- README.md
+++ README.md
@@ -2,40 +2,41 @@
 
 ## The Sieve of Eratosthenes
 
-Prime numbers have many modern day applications and a long history in
-mathematics. Utilizing your own resources, research the sieve of Eratosthenes,
-an algorithm for generating prime numbers. Based on your research, implement
-an API that allows the caller to retrieve the Nth prime number.
-Some stub code and a test suite have been provided as a convenience. However,
-you are encouraged to deviate from Eratosthenes's algorithm, modify the
-existing functions/methods, or anything else that might showcase your ability;
-provided the following requirements are satisfied.
+Using **Python**, this project implements a Sieve of Eratosthenes to compute the **Nth prime number** using a segmented sieve and memory optimizations.
 
-You must author your work in Go, JavaScript/TypeScript, Python, or C# - all
-other language submissions will be rejected. Stub code has been provided, so
-please choose from one of the provided language stubs that is most
-relevant to your skill set and the position you are applying for.
+### O(n) 
+* ~O(nloglog(n))
+
+### Approach
+1. Segmented Sieve - We generate an initial partial list of Primes to start with and utilize a segmented sieve in favor of memory usage and scalability.
+2. bytearray Optimization - a `bytearray` is used instead of a boolean list for efficient memory usage.
+3. Approximation Of Nth Prime - We utilize n(log(n) + loglog(n)) to have the attribute of being > the actual target Prime Number's value.
+
+### Testing Strategy
+
+* Unit Tests
+  * Known prime values
+  * Edge cases
+  * Boundary cases
+  * Data-driven Testing
+* Fuzz Testing
+  * Random values of `n`
+
+### Design decisions
+* Used segmented sieve to handle large values more efficiently
+* used bytearray for memory optimization
+* Used brute-force and filepath for fuzz testing validation
+
+### Future optimizations
+* Adjust for odds-only indexing
+* Static segment size
 
 ### Requirements
+* Python 3.11+
+* Docker (optional)
 
-- Click on the "Use this template" button to create a new GitHub repository, in which you may implement your solution
-- The library package provides an API for retrieving the Nth prime number using 0-based indexing where the 0th prime number is 2
-- Interviewers must be able to execute a suite of tests
-  - Go: `go test ./...`
-    - Use `go test ./... -fuzz=.` to enable fuzz tests (random, potentially invalid inputs)
-  - C#: `dotnet test Sieve.Tests`
-  - Javascript: `npm run test`
-  - Python: `python -m unittest test_sieve.py`
-- Your solution is committed to your project's `main` branch, no uncommitted changes or untracked files please
-- Submit the link to your public repo for review
+### Run tests with Docker
 
-### Considerations
+* **Build image**: docker build -t sieve-tests .
 
-You may add more tests or restructure existing tests, but you may NOT change or remove
-the existing test outcomes; eg- f(0)=2, f(19)=71, f(99)=541, ..., f(10000000)=179424691
-
-During the technical interview, your submission will be discussed, and you will be evaluated in the following areas:
-
-- Technical ability
-- Communication skills
-- Work habits and complementary skills
+* **Run tests**: docker run --rm sieve-tests
--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,42 +1,44 @@
 import math
 
 class Sieve:
-    """
-    Sieve of Eratosthenes
-        I came here to find the 0-indexed Nth prime number and chew bubble gum.
-        And I'm all out of bubble gum.
-    """
 
     def __init__(self) -> None:
         pass
 
     @staticmethod
     def find_upper_limit(n: int) -> int:
+        """
+        Applying the Prime Number Theorem for the approximation for the nth prime number nlog(n).
+        The approx nlog(n) can fall below the actual prime number, so we add a loglog(n) buffer.
+        https://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
+
+        :param n: The 0-indexed Nth target number.
+        :return: The approximation of the Prime Number's value,
+                 with the attribute of being strictly > actual prime value.
+        """
         return int(n * (math.log(n) + math.log(math.log(n))))
 
-    @staticmethod
-    def generate_primes(limit):
 
-        # Using bytearray instead of boolean array to reduce time for _challenging_ amounts of n
+    @staticmethod
+    def generate_partial_prime_list(limit: int) -> list[int]:
+        """
+        We generate a partial primes list up to the sqrt(limit) in favor of using less memory for larger amounts of N.
+
+        :param limit: A number > the actual prime number value.
+        :return: A list of primes up to the sqrt(limit).
+        """
         is_prime = bytearray([1]) * limit
 
-        # First two indexes are not prime
         is_prime[0] = 0
         is_prime[1] = 0
 
-        # We go up to sqrt(limit) due to properties of Prime Numbers
-        # There are no factors of a prime greater than sqrt(limit), except for the number itself
         for i in range(2, int(math.sqrt(limit))):
             if is_prime[i]:
-                # We zero-out all factors of i up to the limit
-                # (i * i) because we already handled i to (i*i) in previous iterations of this loop
-                # Starting at (2 * i) would work, but we would be taking action on elements already actioned upon
                 for j in range(i * i, limit, i):
                     is_prime[j] = 0
 
-        # is_prime is now a full indexed list of all numbers up to limit, marking the Primes and Not-Primes
-        # Now we need to make a list of onlyPrimes
         primes = []
+
         for i in range(2, limit):
             if is_prime[i]:
                 primes.append(i)
@@ -45,87 +47,59 @@ class Sieve:
 
     def nth_prime(self, n: int) -> int:
         """
-        Returns the 0-indexed Nth prime number's value
-        """
+        Returns the Nth prime number's numerical value.
 
+        :param n: The 0-indexed Nth target number.
+        :return: The Nth prime number's value.
+        """
         if n < 0:
             return -1
 
-
         # Small n catches
-        # n < 13 chosen due to upper_limit formula needing a buffer until the 0-indexed 13th prime number
+        # n < 13 chosen due to find_upper_limit() formula needing a buffer until the 0-indexed 13th prime number
         #   n=12, prime_value=41, upper_limit=40, fails up to here
         #   n=13, prime_value=43, upper_limit=45, succeeds going forward
         if n < 13:
             return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41][n]
 
-
-        # Converting to 1-index
         target = n + 1
 
+        upper_limit = self.find_upper_limit(target) # n(log(n) + loglog(n))
 
-        # Apply Prime Number Theorem to get an upper limit of what the nth prime _could_ be
-        # Finds upper limit starting from the 0-index 13th prime number
-        upper_limit = self.find_upper_limit(target) # return int(n * (math.log(n) + math.log(math.log(n))))
+        primes_list = self.generate_partial_prime_list(upper_limit)
 
-        # Generate a list of only Prime Numbers up to limit
-        primes_list = self.generate_primes(upper_limit)
-
-
-        # something like
-        # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
-
-
-        # A composite number (n = a x b) will have either a or b < sqrt(number)
+        # A composite number (n = a x b) will have either factor a or b < sqrt(n)
         # With our upper_limit, we grab the "upper limit" of the small factor number
-        # We don't need the higher factor number, as that would be covered by other iterations of the sieve loop
-        base_prime_limit = int(math.sqrt(upper_limit))
+        # We don't need the higher factor number, as that would be covered by other iterations of the sieve
+        small_factor_limit = int(math.sqrt(upper_limit))
 
-
-        # Setting up segmented sieve
         prime_count = 0
-        lower_bound = 2
-        higher_bound = lower_bound + base_prime_limit
+        lower_seg_bound = 2
+        higher_seg_bound = lower_seg_bound + small_factor_limit
 
+        while lower_seg_bound < upper_limit:
 
-        # Segmented sieve loop
-        while lower_bound < upper_limit:
+            if higher_seg_bound > upper_limit:
+                higher_seg_bound = upper_limit
 
-            # Ensuring the last segment is the exact size
-            if higher_bound > upper_limit:
-                higher_bound = upper_limit
+            is_prime = bytearray([1]) * (higher_seg_bound - lower_seg_bound)
+            # is_prime = [1] * (higher_seg_bound - lower_seg_bound) # Debugger purposes
 
-
-            # Creating our preliminary segmented list where all elements start as Prime (1)
-            is_prime = bytearray([1]) * (higher_bound - lower_bound)
-
-
-            # Setting all non-prime indexes to 0
             for prime in primes_list:
-                # We want to start at the first multiple of prime inside the current segment
-                # For lower value primes, we'll want to start at prime^2, otherwise use the ceiling division
-                start = max(prime * prime, math.ceil(lower_bound/prime) * prime)
+                if prime*prime > higher_seg_bound:
+                    break
+                start = max(prime * prime, math.ceil(lower_seg_bound/prime) * prime)
+                for j in range(start, higher_seg_bound, prime):
+                    is_prime[j-lower_seg_bound] = 0
 
-
-                # Crossing off primes starting from first multiple of prime > lower_bound
-                for j in range(start, higher_bound, prime):
-                    is_prime[j-lower_bound] = 0
-
-
-            # We now have a full segment identifying Prime and Non-Prime indexes
-            # Go through each element in the segment
-            # Increase prime_count for each prime we find, to account for previous segments
-            # If count reaches our target n (Nth Prime), Success, return the index (Prime number)
-            for i in range(lower_bound, higher_bound):
-                if is_prime[i - lower_bound]:
+            for i in range(lower_seg_bound, higher_seg_bound):
+                if is_prime[i - lower_seg_bound]:
                     if prime_count == n:
                         return i
                     prime_count += 1
 
-
-            # Moving onto the next segment
-            lower_bound = higher_bound
-            higher_bound += base_prime_limit
+            lower_seg_bound = higher_seg_bound
+            higher_seg_bound += small_factor_limit
 
         return -1
 
